174e05ee76496b8020b44aaf6cc1f0c2eafe721e
Nicola Redfern <nikkijredfern@gmail.com>
2019-12-29 19:46:38 +1000

    forgot to save.

--- code_wars_who_likes_it.py
+++ code_wars_who_likes_it.py
@@ -8,7 +8,7 @@ def likes(names):
     elif len(names) == 3:
         return names[0] + ", " + names[1] + " and " + names[2] + " like this"
     else:
-        return names[0] + ", " + names[1] + " and " + str((len(names) - 2)) + " like this"
+        return names[0] + ", " + names[1] + " and " + str((len(names) - 2)) + " others like this"
 
 
 print(likes([]))
@@ -16,3 +16,14 @@ print(likes(['Peter']))
 print(likes(['Jacob', 'Alex']))
 print(likes(['Max', 'John', 'Mark']))
 print(likes(['Alex', 'Jacob', 'Mark', 'Max']))
+
+#smart solution below:
+def likes(names):
+    n = len(names)
+    return {
+        0: 'no one likes this',
+        1: '{} likes this',
+        2: '{} and {} like this',
+        3: '{}, {} and {} like this',
+        4: '{}, {} and {others} others like this'
+    }[min(4, n)].format(*names[:3], others=n-2)
