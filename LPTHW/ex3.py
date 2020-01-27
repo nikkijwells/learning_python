901eb6824e94e002123d5706e5b35a79023259ec
Nicola Redfern <nikkijredfern@gmail.com>
2020-01-27 19:47:13 +1000

    Some easy exercises on python.

---
+++ LPTHW/ex3.py
@@ -0,0 +1,27 @@
+#The below is a statement to explain what comes next to the user of this file.
+print("I will now count my chickens:")
+#This is me counting my Hens
+print("Hens:", 25 + 30 /6)
+#This is me counting my Roosters
+print("Roosters:", 100 -25 * 3 % 4)
+#This is me counting the eggs the Hens have laid
+print("Now I will count the eggs:")
+#The below is the calculation for how many eggs I have. No idea why it's been made so complicated for some silly eggs.
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#This is a print statement for some boolean calculations
+print("Is it true that 3 + 2 < 5 - 7?")
+#This is the sum for the boolean calculations
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+#This is a print statement for some boolean calculations
+print("Is it greater?", 5 > -2)
+#This is a print statement for some boolean calculations
+print("Is it greater or equal?", 5 >= -2)
+#This is a print statement for some boolean calculations
+print("Is it less or equal?", 5 <= -2)
